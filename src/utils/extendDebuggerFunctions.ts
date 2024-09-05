23a1c6830957c6d154817c58115897f8fd1332cc
Edie Lemoine <edie@myparcel.nl>
2024-09-05 14:04:50 +0200

    refactor: remove debug statement

--- src/utils/extendDebuggerFunctions.ts
+++ src/utils/extendDebuggerFunctions.ts
@@ -5,18 +5,6 @@ import {type Debugger} from 'debug';
 export const extendDebuggerFunctions = <Options extends BaseOptions>(options: Options, debug: Debugger): CustomDebugger => {
   const logFromVerbosity = (verbosity: number): (formatter: any, ...args: any[]) => void => {
     return (formatter: any, ...args: any[]): void => {
-
-      const filterString = (string: string): boolean => {
-        return debug.namespace.includes(string) ||
-          formatter?.toString().includes(string) ||
-          args.toString().includes(string);
-      };
-
-      // TODO REMOVE THIS!
-      if (!filterString('PdkPlatformName')) {
-        return;
-      }
-
       if (options.verbose < verbosity) {
         return;
       }
