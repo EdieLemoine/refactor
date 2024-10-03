f39cf25ccc21c0e5df670831a17051120d179040
Edie Lemoine <edie@myparcel.nl>
2024-10-03 15:09:56 +0200

    fix: fix importing compiler options

--- src/utils/ts/getCompilerOptions.ts
+++ src/utils/ts/getCompilerOptions.ts
@@ -2,45 +2,43 @@ import type {CommandContext, RefactorOptions} from '../../types.ts';
 import ts, {type CompilerOptions} from 'typescript';
 import {COMPILER_OPTIONS} from '../../constants.ts';
 import {memoize} from '../memoize.ts';
+import path from 'node:path';
 
-const getAllCompilerOptions = (filename: string): CompilerOptions => {
+const getCompilerOptionsRecursive = (filename: string): CompilerOptions => {
   const config = ts.readConfigFile(filename, ts.sys.readFile).config;
 
-  let compilerOptions = { ...config.compilerOptions };
-
-  const extended: string[] = Array.isArray(config.extends) ? config.extends : [config.extends];
-
-  if (extended.length) {
-    extended.forEach((extend) => {
-      if (!extend.startsWith('.')) {
-        return;
-      }
-
-      const requirePath = require.resolve(extend);
-
-      compilerOptions = {
-        ...compilerOptions,
-        ...getAllCompilerOptions(requirePath),
-      };
-    });
+  if (!config.extends) {
+    return config.compilerOptions;
   }
 
-  return {
-    ...compilerOptions,
-    moduleResolution: ts.ModuleResolutionKind.NodeNext,
-    allowArbitraryExtensions: true,
-  };
+  const allExtends: string[] = Array.isArray(config.extends) ? config.extends : [config.extends];
+
+  return allExtends.reduce((acc, extend) => {
+    // No way to resolve non-relative paths currently
+    if (!extend.startsWith('.')) {
+      return acc;
+    }
+
+    const requirePath = path.resolve(path.dirname(filename), extend);
+
+    return {
+      ...acc,
+      ...getCompilerOptionsRecursive(requirePath),
+    };
+  }, config.compilerOptions as CompilerOptions);
 };
 
 const memoized = memoize(((context) => {
   console.log(context.inputPath);
+
   const existingTsConfig = ts.findConfigFile(context.inputPath, (filename) => {
-    console.log('file exists?', filename);
+    console.log('file exists?', filename, ts.sys.fileExists(filename));
+
     return ts.sys.fileExists(filename);
   }, context.options.tsconfigFilename);
 
   return existingTsConfig
-    ? getAllCompilerOptions(existingTsConfig)
+    ? getCompilerOptionsRecursive(existingTsConfig)
     : COMPILER_OPTIONS;
 }) satisfies typeof getCompilerOptions, (context) => context.inputPath);
 
